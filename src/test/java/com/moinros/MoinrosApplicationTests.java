88eb59082d739d8a978c9020945ad1bda66d0724
moinros <1785793169@qq.com>
2020-01-30 15:28:18 +0800

    第一次提交项目到Git仓库 项目创建

---
+++ src/test/java/com/moinros/MoinrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moinros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoinrosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
